9a0f2df01eafc13dfc9baac4492182c289a2456f
Payal Mohapatra <payal@ideas2.research.northwestern.edu>
2024-07-25 01:00:31 -0500

    Adding the unified model

---
+++ audio_helper_functions.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchaudio
+import sys
+
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+
+from tqdm import tqdm
+
+import os
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import os
+import glob
+from torch.utils.tensorboard import SummaryWriter
+import random
+from tqdm import tqdm
+
+
+target_sample_rate = 16000
+num_samples = target_sample_rate * 3
+def _cut_if_necessary(signal):
+    if signal.shape[1] > num_samples:
+        signal = signal[:, :num_samples]
+
+    return signal
+
+def _right_pad_if_necessary(signal):
+    length_signal = signal.shape[1]
+
+    if length_signal < num_samples:
+        num_missing_samples = num_samples - length_signal
+        last_dim_padding = (0, num_missing_samples)
+        signal = torch.nn.functional.pad(signal, last_dim_padding)
+
+    return signal
+
+def _resample_if_necessary(signal, sr):
+    if sr != target_sample_rate:
+        resampler = torchaudio.transforms.Resample(sr, target_sample_rate)
+        signal = resampler(signal)
+    return signal
+
+def _mix_down_if_necessary(signal):
+    if signal.shape[0] > 1:
+        signal = torch.mean(signal, dim=0, keepdim=True)
+    return signal
+
+
